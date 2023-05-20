9f778a1a00da6e55121bb258949ad03a72c71015
HPHuerta <hhernanp@gmail.com>
2023-05-19 23:14:37 -0400

    feat: added hooks to separate useEffects and API calls

--- src/components/IpInformation/IpInformation.tsx
+++ src/components/IpInformation/IpInformation.tsx
@@ -1,34 +1,7 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { type IpType } from 'components/lib/types/types'
-import { useIpData } from 'components/lib/store/ipData'
-import { useLocationCoordinates } from 'components/lib/store/locationCoordinates'
+import { useIpInfo, type IpInfoType } from 'components/lib/hooks/useIpInfo'
 
 export default function IpInformation () {
-  const { ip } = useIpData()
-  const { setLat, setLng } = useLocationCoordinates()
-  const [ipData, setIpData] = useState<IpType | null >(null)
-  const regionString = ipData?.location.region
-  const KEY = process.env.NEXT_PUBLIC_KEY_API as string
-  const URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${KEY}&ipAddress=${ip}`
-
-  const getIpInfo = async () => {
-    // make a request to the ip geolocation api
-    try {
-      const response = await axios.get(URL)
-      if (response.data) {
-        setIpData(response.data)
-        setLat(response.data.location.lat)
-        setLng(response.data.location.lng)
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    getIpInfo()
-  }, [URL])
+  const { ipData, regionString } = useIpInfo() as IpInfoType
 
   return (
     <section className="absolute -top-24 z-10 w-full p-[1.5rem]">
