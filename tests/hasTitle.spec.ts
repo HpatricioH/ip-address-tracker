04edddc2aed103fb89add16d108cd2baa5b7a61d
HPHuerta <hhernanp@gmail.com>
2023-05-21 00:34:07 -0400

    feat: added new tests using playwright

--- tests/hasTitle.spec.ts
+++ tests/hasTitle.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
+  await page.goto('https://ip-address-tracker-five-mauve.vercel.app/')
   const title = page.locator('text=IP Address Tracker')
   await expect(title).toContainText('IP Address Tracker')
 })
